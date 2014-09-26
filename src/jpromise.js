48db81b648f3444e4a6c73810fa86d3c9a9b6590
qiqiboy <imqiqiboy@gmail.com>
2014-09-26 20:16:11 +0800

    update

--- src/jpromise.js
+++ src/jpromise.js
@@ -62,7 +62,7 @@
             }
             return this;
         },
-        then:function(resolve,reject){
+        then:function(){
             var next=new struct,
                 fns=arguments;
             
